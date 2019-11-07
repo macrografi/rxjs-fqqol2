3b31e2e4d1a10fd4cb47d7a86c6593eee147e4ee
macrografi <macrografi@hotmail.com>
2019-11-07 15:34:14 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { of, from } from "rxjs";
-import { map, tap, take, filter } from "rxjs/operators";
+import { map, tap, take, filter, concat, catchError } from "rxjs/operators";
 
 /*
 //RxJS Operator: MAP;
@@ -42,10 +42,50 @@ of(2, 4, 6)
 */
 
 /*
-//RxJS Operator: TAKE;
+//RxJS Operator: FILTER;
 of(1, 2, 3, 4, 5)
   .pipe(filter(item => item % 2 === 0))
   .subscribe(console.log);
 
   //result: 2,4
 */
+
+
+ /*
+//RxJS Operator: CONCAT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!;
+let  argument1 = of(1, 2, 3);
+let  argument2 = of(4, 5, 6);
+let  argument3 = of(7, 8, 9);
+
+let  result = argument1.pipe(concat(argument2));
+result = result.pipe(concat(argument3));
+
+result.subscribe(console.log)
+
+//of(result).subscribe(console.log);
+
+*/
+
+let  argument1 = of(1, 2, 3);
+let  argument2 = of(4, 5, 6);
+let  argument3 = of(7, 8, 9);
+let  result = argument1.pipe(concat(argument2.pipe(concat(argument3))));
+result.subscribe(console.log)
+
+/*
+//RxJS Operator: CATCHERROR;
+of(8, 6, 4, 2)
+  .pipe(
+    map(item => {
+      if (item === 2){
+        throw "";
+      }
+      return item / (item - 2);
+    }),
+    catchError(err => of("Bölünme Hatası"))
+  )
+  .subscribe(console.log);
+
+  //result: 1.3333333333333333, 1.5, 2, Bölünme Hatası;
+
+  */
