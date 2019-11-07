185675810bf49cd52ff474ec9addaa07c2d9124e
macrografi <macrografi@hotmail.com>
2019-11-07 09:54:18 +0300

    1 file modified

--- index.ts
+++ index.ts
@@ -1,10 +1,24 @@
 import { of, from } from "rxjs";
 import { map, tap, take } from "rxjs/operators";
 
+/*
 of(2, 4, 6).pipe(
   map(item => item * 2)
 ).subscribe(console.log);
 
-/*
   result:4,8,12 
 */
+
+/*
+of(2, 4, 6)
+  .pipe(
+    tap(item => console.log(item)),
+    map(item => item * 2),
+    tap(item => console.log(item)),
+    map(item => item - 3),
+    tap(item => console.log(item))
+  )
+  .subscribe();
+
+  //result: 2,4,1,4,8,5,6,12,9
+  */
